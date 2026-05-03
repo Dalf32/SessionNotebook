d22efc5a66c5c71f4be6266459b8b265e63d0087
Dalf32 <kylepmullins@gmail.com>
2026-05-03 19:21:32 -0400

    missing file

---
+++ SessionNotebook/Data/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace SessionNotebook;
+
+public static class StringExtensions
+{
+    public static string Capitalize(this string str)
+    {
+        if (str == string.Empty)
+        {
+            return str;
+        }
+
+        if (str.Length == 1)
+        {
+            return str.ToUpper();
+        }
+
+        return char.ToUpper(str[0]) + str[1..].ToLower();
+    }
+}
